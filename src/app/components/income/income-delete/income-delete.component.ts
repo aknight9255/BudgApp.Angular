3c80418bcc9ce22bf68e4d9192cc77009f893e53
aknight9255 <amanda.knight55@gmail.com>
2019-08-07 11:07:06 -0400

    stubbed out folders

---
+++ src/app/components/income/income-delete/income-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-income-delete',
+  templateUrl: './income-delete.component.html',
+  styleUrls: ['./income-delete.component.css']
+})
+export class IncomeDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/service/auth.service/auth.service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth.service',
+  templateUrl: './auth.service.component.html',
+  styleUrls: ['./auth.service.component.css']
+})
+export class Auth.ServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
