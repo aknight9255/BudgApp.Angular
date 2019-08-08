bbc7afab3ab742bd20c286bd02c4913e03ba79e0
Chase00 <chasekuhnmc@gmail.com>
2019-08-08 13:07:07 -0400

    Category index page

--- src/app/components/category/category-index/category-index.component.ts
+++ src/app/components/category/category-index/category-index.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/service/category.service';
+import { Category } from 'src/app/models/category';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-category-index',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryIndexComponent implements OnInit {
 
-  constructor() { }
+  columnNames = ['details', 'CategoryID', 'CategoryType', 'buttons'];
+  dateSource: MatTableDataSource<Category>;
+
+  constructor(private _categoryService: CategoryService) { }
 
   ngOnInit() {
+    this._categoryService.getCategory().subscribe((categories: Category[]) => { // On init, retreives all the categories from the API
+      this.dateSource = new MatTableDataSource<Category>(categories);
+    });
+
   }
 
 }
