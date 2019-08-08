591ed9f7d2b50c167af6d5efde4fa7d9cbcfe78f
aknight9255 <amanda.knight55@gmail.com>
2019-08-08 09:30:52 -0400

    login page finished !not styled!

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private _loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private _form: FormBuilder, private _authService: AuthService) { 
+    this.createForm();
+  }
 
   ngOnInit() {
   }
 
+  createForm(){
+    this._loginForm = this._form.group({
+      email: new FormControl,
+      password: new FormControl
+    });
+  }
+
+  onSubmit(){
+    console.log(this._loginForm.value);
+    this._authService.login(this._loginForm.value);
+  }
+
 }
+
