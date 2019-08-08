9014f933bdc882c4fb5fbb9c5f517e8cb68e759e
ShabnamZ <shabnam.ziyovuddinova@gmail.com>
2019-08-08 18:14:24 -0400

    Category delete added

--- src/app/components/category/category-delete/category-delete.component.ts
+++ src/app/components/category/category-delete/category-delete.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+  import { Component, OnInit } from '@angular/core';
+  import { CategoryService } from 'src/app/service/category.service';
+  import { ActivatedRoute, Router } from '@angular/router';
+  import { Category } from 'src/app/models/category';
+  
+  @Component({
+    selector: 'app-category-delete',
+    templateUrl: './category-delete.component.html',
+    styleUrls: ['./category-delete.component.css'] })
 
-@Component({
-  selector: 'app-category-delete',
-  templateUrl: './category-delete.component.html',
-  styleUrls: ['./category-delete.component.css']
-})
-export class CategoryDeleteComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  export class CategoryDeleteComponent implements OnInit {
+    category: Category;
+  
+    constructor(private _categoryService: CategoryService, private _ar: ActivatedRoute, private _router: Router) {
+      this._ar.paramMap.subscribe(p => {
+        this._categoryService.getCategory(p.get('id')).subscribe((singleCategory: Category) => {
+          this.category = singleCategory;
+        });
+      });
+     }
+  
+  
+    ngOnInit() {
+    }
+  
+    onDelete(){
+      this._categoryService.deleteCategory(this.category.CategoryID).subscribe(()=> {
+        this._router.navigate(['/category']);
+      });
+    }
+  
   }
-
-}
