cabcac4cf6a02937534ff4130367783c9ab8d506
aknight9255 <amanda.knight55@gmail.com>
2019-08-13 19:36:38 -0400

    balance function added

--- src/app/components/income/cards/cards.component.ts
+++ src/app/components/income/cards/cards.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TransactionService } from 'src/app/service/transaction.service';
+import { MatTableDataSource} from '@angular/material';
+import { Transaction } from '../../../models/transaction';
 
 @Component({
   selector: 'app-cards',
@@ -7,15 +10,59 @@ import {Component} from '@angular/core';
 })
 export class Cards {
   today: number = Date.now();
-  monthTwo: number = new Date().getMonth() +1;
+
+  monthTwo: number = new Date().getMonth() + 2;
   thisYear: number = new Date().getFullYear();
-  fullDateMonthTwo: Date = new Date(this.thisYear, this.monthTwo, 1);
+  fullDateMonthTwo: string = `${this.monthTwo}/01/${this.thisYear}`;
   
-  monthThree: number = new Date().getMonth() + 2;
-  fullDateMonthThree: Date = new Date(this.thisYear, this.monthThree,1);
-  constructor() { }
+  monthThree: number = new Date().getMonth() + 3;
+  fullDateMonthThree: string = `${this.monthThree}/01/${this.thisYear}`;
+  
+  monthOne: number = new Date().getMonth() + 1;
+  fullDateMonthOne: string = `${this.monthOne}/01/${this.thisYear}`;
+  columnNames = ['CategoryType', 'TransactionAmount', 'TransactionDate', 'buttons']
+  dataSourceOne: MatTableDataSource<Transaction>
+  dataSourceTwo: MatTableDataSource<Transaction>
+  dataSourceThree: MatTableDataSource<Transaction>
+  balanceOne: number;
+  balanceTwo: number;
+  balanceThree: number;
+  
+    constructor(private _transactionService: TransactionService) { }
+  
+    ngOnInit() {
+      console.log(this.fullDateMonthOne);
+      this._transactionService.getTransactionByMonth(this.fullDateMonthOne).subscribe((transactions: Transaction[]) => {
+        this.dataSourceOne = new MatTableDataSource<Transaction>(transactions);
+        console.log(transactions)
+      });
+  
+      this._transactionService.getTransactionByMonth(this.fullDateMonthTwo).subscribe((transactions: Transaction[]) => {
+        this.dataSourceTwo = new MatTableDataSource<Transaction>(transactions);
+        console.log(transactions)
+      });
+  
+      this._transactionService.getTransactionByMonth(this.fullDateMonthThree).subscribe((transactions: Transaction[]) => {
+        this.dataSourceThree = new MatTableDataSource<Transaction>(transactions);
+        console.log(transactions)
+      });
+      
+      this._transactionService.pullBalance((this.fullDateMonthOne)).subscribe((singleBalance: number) => {
+        this.balanceOne = singleBalance;
+        console.log(singleBalance)
+      });
 
-  ngOnInit() {
+      this._transactionService.pullBalance((this.fullDateMonthTwo)).subscribe((singleBalance: number) => {
+        this.balanceTwo = singleBalance;
+        console.log(singleBalance)
+      });
+
+      this._transactionService.pullBalance((this.fullDateMonthThree)).subscribe((singleBalance: number) => {
+        this.balanceThree = singleBalance;
+        console.log(singleBalance)
+      });
+      
+    }
+  
   }
-
-}
+  
--- src/app/components/transaction/transaction-index/transaction-index.component.ts
+++ src/app/components/transaction/transaction-index/transaction-index.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TransactionService } from 'src/app/service/transaction.service';
 import { MatTableDataSource} from '@angular/material';
 import { Transaction } from '../../../models/transaction';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-transaction-index',
@@ -9,42 +10,17 @@ import { Transaction } from '../../../models/transaction';
   styleUrls: ['./transaction-index.component.css']
 })
 export class TransactionIndexComponent implements OnInit {
-today: number = Date.now();
 
-monthTwo: number = new Date().getMonth() + 2;
-thisYear: number = new Date().getFullYear();
-fullDateMonthTwo: string = `${this.monthTwo}/01/${this.thisYear}`;
-
-monthThree: number = new Date().getMonth() + 3;
-fullDateMonthThree: string = `${this.monthThree}/01/${this.thisYear}`;
-
-monthOne: number = new Date().getMonth() + 1;
-fullDateMonthOne: string = `${this.monthOne}/01/${this.thisYear}`;
 columnNames = ['CategoryType', 'TransactionAmount', 'TransactionDate', 'buttons']
-dataSourceOne: MatTableDataSource<Transaction>
-dataSourceTwo: MatTableDataSource<Transaction>
-dataSourceThree: MatTableDataSource<Transaction>
 
-  constructor(private _transactionService: TransactionService) { }
+
+dataSource: MatTableDataSource<Transaction>
+
+  constructor(private _transactionService: TransactionService, private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    console.log(this.fullDateMonthOne);
-    this._transactionService.getTransactionByMonth(this.fullDateMonthOne).subscribe((transactions: Transaction[]) => {
-      this.dataSourceOne = new MatTableDataSource<Transaction>(transactions);
-      console.log(transactions)
+    this._transactionService.getTransactions().subscribe((transactions: Transaction[]) => {
+      this.dataSource = new MatTableDataSource<Transaction>(transactions);
     });
-
-    this._transactionService.getTransactionByMonth(this.fullDateMonthTwo).subscribe((transactions: Transaction[]) => {
-      this.dataSourceTwo = new MatTableDataSource<Transaction>(transactions);
-      console.log(transactions)
-    });
-
-    this._transactionService.getTransactionByMonth(this.fullDateMonthThree).subscribe((transactions: Transaction[]) => {
-      this.dataSourceThree = new MatTableDataSource<Transaction>(transactions);
-      console.log(transactions)
-    });
-    
-    
   }
-
 }
