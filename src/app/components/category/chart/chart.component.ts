cc3340bc142a7b150d84651b2790ed3f1ff15596
Chase00 <chasekuhnmc@gmail.com>
2019-08-13 10:41:40 -0400

    added basic chart

---
+++ src/app/components/category/chart/chart.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.css']
+})
+export class ChartComponent implements OnInit {
+
+  constructor() { }
+
+  chartData = [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45]
+}]
+
+  ngOnInit() {
+    
+  }
+
+}
