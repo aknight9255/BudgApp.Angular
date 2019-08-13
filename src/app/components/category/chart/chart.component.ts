a7c7f7329cda58c7599ebd32da4036b817cd529c
ShabnamZ <shabnam.ziyovuddinova@gmail.com>
2019-08-13 15:06:15 -0400

    Datepicker added to Transaction edit

--- src/app/components/category/chart/chart.component.ts
+++ src/app/components/category/chart/chart.component.ts
@@ -9,28 +9,13 @@ export class ChartComponent implements OnInit {
 
   constructor() { }
 
-//   chartData = [{
-//     label: 'My First dataset',
-//     backgroundColor: 'rgb(255, 99, 132)',
-//     borderColor: 'rgb(255, 99, 132)',
-//     data: [0, 10, 5, 2, 20, 30, 45]
+  chartData = [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45]
 
-//   }]
-//       function addData(chart, label, data) {
-//         chart.data.labels.push(label),
-//         chart.data.datasets.forEach((dataset) => {
-//             dataset.data.push(data),
-//         });
-//         chart.update(),
-  
-//   function removeData(chart) {
-//       chart.data.labels.pop();
-//       chart.data.datasets.forEach((dataset) => {
-//           dataset.data.pop();
-//       });
-//       chart.update();
-//   }
-// }];
+  }]
 
   ngOnInit() {
     
