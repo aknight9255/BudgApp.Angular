ef27feb26b21c09b6839b2c91c54e9307970d30b
aknight9255 <amanda.knight55@gmail.com>
2019-08-15 10:53:53 -0400

    test123

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ switch (window.location.hostname) {
     // this is the deployed angular application
     case 'tbc-budgapp.herokuapp.com':
         // this is the full url of your deployed backend API
-        APIURL = 'https://budgapp.azurewebsites.net'
+        APIURL = 'https://budgapp.azurewebsites.net/'
         break;
     default:
         // this is the local host name of your API
