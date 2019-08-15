1e42dba493175f19617d2cbf7bbe3d02be980507
aknight9255 <amanda.knight55@gmail.com>
2019-08-15 09:51:02 -0400

    first heroku deployment

--- src/app/service/category.service.ts
+++ src/app/service/category.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Category } from '../models/category';
+import { APIURL } from 'src/environments/environment.prod';
 
-const ApiUrl = 'https://budgapp.azurewebsites.net/api'
+// const ApiUrl = 'https://budgapp.azurewebsites.net/api'
 
 @Injectable()
 export class CategoryService {
@@ -10,22 +11,22 @@ export class CategoryService {
   constructor(private _http: HttpClient) { }
 
   getCategories(): any {
-    return this._http.get(`${ApiUrl}/Category`, { headers: this.getHeaders() });
+    return this._http.get(`${APIURL}/api/Category`, { headers: this.getHeaders() });
   }
   getCategory(id: string) {
-    return this._http.get(`${ApiUrl}/Category/${id}`, { headers: this.getHeaders() });
+    return this._http.get(`${APIURL}/api/Category/${id}`, { headers: this.getHeaders() });
   }
 
   createCategory(category: Category){
-    return this._http.post(`${ApiUrl}/Category`,category , {headers: this.getHeaders() });
+    return this._http.post(`${APIURL}/api/Category`,category , {headers: this.getHeaders() });
   }
 
   updateCategory(category: Category){
-    return this._http.put(`${ApiUrl}/Category`,category , {headers: this.getHeaders() });
+    return this._http.put(`${APIURL}/api/Category`,category , {headers: this.getHeaders() });
   }
 
   deleteCategory(id: number) {
-    return this._http.delete(`${ApiUrl}/Category/${id}`, { headers: this.getHeaders() });
+    return this._http.delete(`${APIURL}/api/Category/${id}`, { headers: this.getHeaders() });
   }
   
   private getHeaders() {
--- src/app/service/transaction.service.ts
+++ src/app/service/transaction.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Transaction } from '../models/transaction';
-const Api_Url = 'https://budgapp.azurewebsites.net/api';
+import { APIURL } from 'src/environments/environment.prod';
+
+// const Api_Url = 'https://budgapp.azurewebsites.net/api';
 
 @Injectable()
 export class TransactionService {
@@ -9,37 +11,37 @@ export class TransactionService {
   constructor(private _http: HttpClient) { }
 
   getTransactions(){
-    return this._http.get(`${Api_Url}/Transaction`, {headers: this.getHeaders()});
+    return this._http.get(`${APIURL}/api/Transaction`, {headers: this.getHeaders()});
   }
   
   getTransaction(id: string){
-    return this._http.get(`${Api_Url}/Transaction/${id}`, { headers: this.getHeaders() });
+    return this._http.get(`${APIURL}/api/Transaction/${id}`, { headers: this.getHeaders() });
   }
 
   getTransactionByMonth(coolDate: string){
-    return this._http.get(`${Api_Url}/Transaction?coolDate=${coolDate}`, { headers: this.getHeaders() });
+    return this._http.get(`${APIURL}/api/Transaction?coolDate=${coolDate}`, { headers: this.getHeaders() });
   }
 
   createTransaction(transaction: Transaction){
-    return this._http.post(`${Api_Url}/Transaction`,transaction, { headers: this.getHeaders()});
+    return this._http.post(`${APIURL}/api/Transaction`,transaction, { headers: this.getHeaders()});
   }
 
   updateTransaction(transaction: Transaction){
-    return this._http.put(`${Api_Url}/Transaction`,transaction, { headers: this.getHeaders()});
+    return this._http.put(`${APIURL}/api/Transaction`,transaction, { headers: this.getHeaders()});
   }
 
   pullBalance(monthKey: string){
-    return this._http.get(`${Api_Url}/Transaction/GetBalance?monthKey=${monthKey}`, { headers: this.getHeaders() });
+    return this._http.get(`${APIURL}/api/Transaction/GetBalance?monthKey=${monthKey}`, { headers: this.getHeaders() });
 
   }
 
   pullChartData(): any{
-    return this._http.get(`${Api_Url}/Transaction/GetChartData`, { headers: this.getHeaders() });
+    return this._http.get(`${APIURL}/api/Transaction/GetChartData`, { headers: this.getHeaders() });
 
   }
 
   deleteTransaction(id: number){
-    return this._http.delete(`${Api_Url}/Transaction/${id}`, {headers: this.getHeaders() });
+    return this._http.delete(`${APIURL}/api/Transaction/${id}`, {headers: this.getHeaders() });
 
   }
 
