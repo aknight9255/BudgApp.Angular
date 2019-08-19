09d52c91c51f9416da745192ba84112a737166eb
Chase00 <chasekuhnmc@gmail.com>
2019-08-19 17:09:02 -0400

    Added Login & Register form validation errors

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import {FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
@@ -8,13 +8,42 @@ import { AuthService } from 'src/app/service/auth.service';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+email = new FormControl('', [Validators.required, Validators.email]);
+password = new FormControl('', [Validators.required]);
+confirmpassword = new FormControl('', [Validators.required]);
+
 _registerForm: FormGroup;
+error: boolean;
+message: string;
+
+  getErrorMessageEmail() {
+    return this.email.hasError('required') ? 'You must enter an email' :
+        this.email.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+
+  getErrorMessagePassword() {
+    return this.password.hasError('required') ? 'You must enter a password' :
+        this.password.hasError('password') ? 'Password does not meet requirements ' :
+        '';
+  }
+
+  getErrorMessageConfirmPassword() {
+    return this.confirmpassword.hasError('required') ? 'You must confirm your password' :
+        this.confirmpassword.hasError('password') ? 'Passwords do not match' :
+        '';
+  }
 
   constructor(private _form: FormBuilder,private _authService: AuthService) {
     this.createForm();
    }
 
   ngOnInit() {
+    // this._registerForm = this._form.group({
+    //   email: ['', Validators.required],
+    //   password: ['', Validators.required],
+    //   confirmPassword: ['', Validators.required]
+    // });
   }
 
   createForm(){
@@ -27,8 +56,16 @@ _registerForm: FormGroup;
 
   onSubmit(){
     console.log(this._registerForm.value);
+    this.ErrorMsg();
     this._authService.register(this._registerForm.value).subscribe( () => this._authService.login(this._registerForm.value));
   }
 
+  ErrorMsg(){
+    if (this._registerForm.invalid || this._registerForm) {
+      this.error = true;
+      this.message = "This is an error message"
+      return;
+    }
+  }
 
 }
